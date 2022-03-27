ba6fb8525ac7c240fd20890617a7b17dab4cfea0
Fernando Sanches <ferbs89@gmail.com>
2022-03-27 10:39:48 -0300

    add new field durationInSeconds

--- src/resolvers.js
+++ src/resolvers.js
@@ -15,6 +15,7 @@ const resolvers = {
       return dataSources.trackAPI.getModule(id);
     },
   },
+
   Mutation: {
     // increments a track's numberOfViews property
     incrementTrackViews: async (_, { id }, { dataSources }) => {
@@ -36,6 +37,7 @@ const resolvers = {
       }
     },
   },
+
   Track: {
     author: ({ authorId }, _, { dataSources }) => {
       return dataSources.trackAPI.getAuthor(authorId);
@@ -44,6 +46,8 @@ const resolvers = {
     modules: ({ id }, _, { dataSources }) => {
       return dataSources.trackAPI.getTrackModules(id);
     },
+
+    durationInSeconds: ({ length }) => length,
   },
 };
 
